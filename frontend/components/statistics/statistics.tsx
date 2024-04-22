fc644d07437142e42547db6201fb296d6939c5f4
dancoon <dancun.ian@students.jkuat.ac.ke>
2024-04-22 19:36:00 +0300

    fix: :bug: Fix dark mode

--- frontend/components/statistics/statistics.tsx
+++ frontend/components/statistics/statistics.tsx
@@ -26,25 +26,25 @@ const Statistics = () => {
             tech industry.
           </p>
           <div className="mt-6 flex space-x-6">
-            <Button className="bg-white bg-opacity-0 border border-white rounded-sm">
+            <Button className="bg-white bg-opacity-0 border border-white rounded-sm dark:text-white">
               Learn More
             </Button>
-            <Button className="bg-white bg-opacity-0">
+            <Button className="bg-white bg-opacity-0 dark:text-white">
               Sign Up <FaChevronRight className="ml-1" />
             </Button>
           </div>
         </div>
       </div>
       <div className="mt-10 flex flex-col md:flex-row md:space-y-0 md:justify-between">
-        <div className=" border-l-4 pl-4 md:py-6">
+        <div className=" border-l-4 pl-4 md:py-6 dark:border-white">
           <span className="font-bold text-3xl md:text-5xl">30%</span>
           <p className="font-sm md:font-lg md:mt-3">Developers Connected and Growing</p>
         </div>
-        <div className="mt-6 border-l-4 pl-4 md:py-6">
+        <div className="mt-6 border-l-4 pl-4 md:py-6 dark:border-white">
           <span className="font-bold text-3xl md:text-5xl">30%</span>
           <p className="font-sm md:font-lg md:mt-3">Projects Collaborated On</p>
         </div>
-        <div className="mt-6 border-l-4 pl-4 md:py-6">
+        <div className="mt-6 border-l-4 pl-4 md:py-6 dark:border-white">
           <span className="font-bold text-3xl md:text-5xl">30%</span>
           <p className="font-sm md:font-lg md:mt-3">Community Members Engaged</p>
         </div>
