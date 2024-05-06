27e853f1ece0b30649920848ef26800c196da579
dancoon <dancun.ian@students.jkuat.ac.ke>
2024-05-06 14:55:40 +0300

    fix: :bug: import signals in urls

--- backend/articles/urls.py
+++ backend/articles/urls.py
@@ -1,10 +1,10 @@
 from django.urls import include, path
 from . import views
+from . import signals
 
 app_name = "articles"
 
 urlpatterns = [
     path("", views.articles_list, name="articles-list"),
     path("<slug:slug>/", views.article_detail, name="article-detail"),
-
 ]
