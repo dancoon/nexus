be892a998ec1229f727a6b1df873cdab2478f434
dancoon <dancun.ian@students.jkuat.ac.ke>
2024-05-08 09:55:54 +0300

    feat: :sparkles: Install CKEditor

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -20,6 +20,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 schema_view = get_schema_view(
@@ -46,6 +48,7 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("ckeditor5/", include("django_ckeditor_5.urls")),
     path("", include("users.urls")),
     path("articles/", include("articles.urls", namespace="articles")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
