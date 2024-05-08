a8a790a7a51bca88e62757c5f9cb6a33a307edf9
dancoon <dancun.ian@students.jkuat.ac.ke>
2024-05-08 09:56:26 +0300

    feat: :sparkles: Register Article Model in admin site

--- backend/articles/admin.py
+++ backend/articles/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+@admin.register(models.Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "author"]
+
+
+@admin.register(models.Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
