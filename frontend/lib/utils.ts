7a6952034dc93047220605d7b31362a47f66601e
dancoon <dancun.ian@students.jkuat.ac.ke>
2024-05-09 19:28:08 +0300

    feat: Create extractTextFromHtml util function

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { load } from "cheerio";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -7,4 +8,14 @@ export function cn(...inputs: ClassValue[]) {
 
 export function errorParse(obj: Record<string, string[]>): string {
   return Object.values(obj).flat().join(", ");
+}
+
+export function extractTextFromHTML(htmlString: string): string {
+  if (typeof htmlString !== "string") {
+    throw new Error("Invalid input: htmlString must be a string");
+  }
+
+  const $ = load(htmlString);
+  const text = $("body").text().trim();
+  return text;
 }
